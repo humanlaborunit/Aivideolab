2269373a4529454c27a14ad6d97b2d19552a6ab4
humanlaborunit <humanlaborunit@proton.me>
2025-06-30 12:02:36 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd /app || exit
-echo "Launching Aivideolab NSFW AI Video Engine..."
+
+echo "============== STARTING Aivideolab CONTAINER =============="
+echo "PWD: $(pwd)"
+echo "Contents of /app:"
+ls -la /app
+
+echo "==================== RUNNING APP ==========================="
 python3 run_ui.py --host 0.0.0.0 --port 7860
 
